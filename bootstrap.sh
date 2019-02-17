c95a102b8726ccc83c7628a158025e166fbc6977
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-17 14:29:59 +0530

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,19 +11,21 @@ echo ${username}
 if [ "${authentication}" == "Username:Password" ]
 then
 	password=$4
+	echo $password
 	export SSHPASS=$password
 	sshpass -e ssh-copy-id -i ~/.ssh/bootstrap "${username}"@"${ip}"
 	cp ~/.ssh/bootstrap /tmp/bootstrap_key
 elif [ "${authentication}" == "Username:SSH Key" ]
 then
 	key=$4
+	echo $key
 	echo $key > /tmp/bootstrap_key
 	chmod 600 /tmp/bootstrap_key
 fi
 
 ssh -i /tmp/bootstrap_key "${username}"@"${ip}" hostname
 
-if ["$?" == 0 ]
+if [ "$?" == 0 ]
 then
 	echo "Bootstrap Suuccessful"
 	exit 0
