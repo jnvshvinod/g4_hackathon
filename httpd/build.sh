dde2faa9bdbc5db10433aef6f8c24e425811f99a
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-17 23:14:34 +0530

    final

--- httpd/build.sh
+++ httpd/build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+echo -----------------------------------------------------
+echo "        Preparing Docker Image"
+echo -----------------------------------------------------
 
-sed -i 's/conffile/'"$conffile"'/g' Dockerfile
-sed -i 's/port/'"$port"'/g' Dockerfile
-sed -i 's/documentRoot/'"$documentRoot"'/g' Dockerfile      
+conffile=$(cat ./pull_result.json | jq .conffile| tr -d '"')
+port=$(cat ./pull_result.json | jq .port| tr -d '"')
+documentRoot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
 
-docker build .
+sed -i 's:conffile:'"$conffile"':g' ./httpd/Dockerfile
+sed -i 's:port:'"$port"':g' ./httpd/Dockerfile
+sed -i 's:documentRoot:'"$documentRoot"':g' ./httpd/Dockerfile
+
+cat ./httpd/Dockerfile
+docker build ./httpd/Dockerfile -t httpd_${BUILD_NUMBER}
