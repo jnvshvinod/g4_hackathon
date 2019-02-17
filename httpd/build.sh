c2023dd0dd8fb0eb0aad80d96efca2cd4001d980
root <root@ip-172-31-17-201.ap-south-1.compute.internal>
2019-02-17 17:36:32 +0000

    fix

--- httpd/build.sh
+++ httpd/build.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
-# Script to pull configurations of HTTPD server and build docker image.
-
-echo -----------------------------------------------------
-echo "        Pulling Configurations and Artifacts"
-echo -----------------------------------------------------
-
-# Default system properties, Modify for custom locations, if any
-conffile="/etc/httpd/conf/httpd.conf"
-echo "Taking Configuration from "$conffile
-
-documentRoot=$(cat /etc/httpd/conf/httpd.conf | grep '^\s*DocumentRoot'|tr -d " "|cut -d'"' -f2)
-echo "Taking deployed artifacts from "$documentRoot
-
-port=$(cat $conffile | grep -e '^\s*Listen'|tr -d " "|tr -d "Listen"|head -1)
-echo "Webserver listening at port "$port
-
-echo -----------------------------------------------------
-echo "        Preparing Docker"
-echo -----------------------------------------------------
-
-sed -i 's/conffile/'"conffile"'/g' Dockerfile
+sed -i 's/conffile/'"$conffile"'/g' Dockerfile
 sed -i 's/port/'"$port"'/g' Dockerfile
 sed -i 's/documentRoot/'"$documentRoot"'/g' Dockerfile      
 
---
+++ httpd/pull.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Script to pull configurations of HTTPD server and build docker image.
+
+# Default system properties, Modify for custom locations, if any
+conffile="/etc/httpd/conf/httpd.conf"
+
+documentRoot=$(cat /etc/httpd/conf/httpd.conf | grep '^\s*DocumentRoot'|tr -d " "|cut -d'"' -f2)
+
+port=$(cat $conffile | grep -e '^\s*Listen'|tr -d " "|tr -d "Listen"|head -1)
+
+echo "{ \"conffile\":\"${conffile}\",\"documentRoot\":\"${documentRoot}\",\"port\":\"${port}\"}
