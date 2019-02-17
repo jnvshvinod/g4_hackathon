bdb42f753a688f5b001bebb9ad35bb00585c3596
J N V S H Vinod <vinod_jn@ad.infosys.com>
2019-02-17 20:26:08 +0530

    scan

--- scan.sh
+++ scan.sh
@@ -9,17 +9,23 @@ echo -----------------------------------------------------
 echo "                     SCANNING"
 echo -----------------------------------------------------
 
+result="{\n"
+
 for service in "${services_to_check[@]}"
 do
 echo "Checking for" $service
-if [ "$(systemctl show -p SubState $service| cut -d"=" -f2)" == "running" ]
-then
-  echo $service is running
-setup=$setup" $service"
-else
-  echo $service is not running
-fi
+  if [ "$(systemctl show -p SubState $service| cut -d"=" -f2)" == "running" ]
+  then
+    echo $service is running
+    result=$result+" $service:true,\n" 
+    setup=$setup" $service"
+  else
+    result=$result+" $service:false,\n" 
+    echo $service is not running
+  fi
 done
+result=$result+" $status:completed\n}"
+echo $result
 echo Setup will continue for below web servers.
 echo $setup
 
