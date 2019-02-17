1e6ce3fc449c72aa6fc668539445dc2cc7fb922b
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-17 16:12:51 +0530

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ then
 	password=$4
 	echo $password
 	export SSHPASS=$password
-	sshpass -e ssh-copy-id -i /opt/jenkinsfiles/bootstrap "${username}"@"${ip}"
+	sshpass -e ssh-copy-id -f -i /opt/jenkinsfiles/bootstrap "${username}"@"${ip}"
 	cp /opt/jenkinsfiles/bootstrap /opt/jenkinsfiles/tmp/bootstrap_key_${ip}
 elif [ "${authentication}" == "Username:SSH Key" ]
 then
