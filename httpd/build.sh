6a8d3c16c5ae69e2eedc99e2eb440d07d27493a3
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-17 23:16:26 +0530

    fix

--- httpd/build.sh
+++ httpd/build.sh
@@ -7,9 +7,9 @@ conffile=$(cat ./pull_result.json | jq .conffile| tr -d '"')
 port=$(cat ./pull_result.json | jq .port| tr -d '"')
 documentRoot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
 
-sed -i 's:conffile:'"$conffile"':g' ./httpd/Dockerfile
-sed -i 's:port:'"$port"':g' ./httpd/Dockerfile
-sed -i 's:documentRoot:'"$documentRoot"':g' ./httpd/Dockerfile
+sed -i 's:conffile:'"$conffile"':g' Dockerfile
+sed -i 's:port:'"$port"':g' Dockerfile
+sed -i 's:documentRoot:'"$documentRoot"':g' Dockerfile
 
 cat ./httpd/Dockerfile
-docker build ./httpd/Dockerfile -t httpd_${BUILD_NUMBER}
+docker build . -t httpd_${BUILD_NUMBER}
