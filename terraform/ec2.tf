1ff797bd09e55452590c830ae2e86106cda530e3
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-18 18:15:44 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,12 +3,18 @@ resource "aws_instance" "dest_instance" {
   instance_type               = "t2.medium"
   key_name                    = "G4"
   subnet_id                   = "subnet-2a5b0e42"
+  vpc_security_group_ids      = ["sg-0b46806a9a699bbd7"]
   
   root_block_device {
     volume_type           = "gp2"
     volume_size           = "30"
   }
-
+  
+  user_data = <<-EOF
+                #!/bin/bash
+                yum install -y aws*
+                yum install -y docker
+                EOF
   tags {
     Name = "dest-server"
   }
