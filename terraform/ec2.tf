d65f06619a4eceb8d161f37168a7a217b5bbcc45
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-18 18:31:28 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,7 +13,10 @@ resource "aws_instance" "dest_instance" {
   user_data = <<-EOF
                 #!/bin/bash
                 yum install -y aws*
+                sleep 15
                 yum install -y docker
+                sleep 15
+                sudo systemctl start docker
                 EOF
   tags {
     Name = "dest-server"
