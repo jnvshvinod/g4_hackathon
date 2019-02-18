128ee9006aa4f4ece382ed2352c62b0b0d5f64e6
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-18 11:59:26 +0530

    fix typo

--- httpd/build.sh
+++ httpd/build.sh
@@ -7,10 +7,10 @@ echo -----------------------------------------------------
 scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ./httpd/pull.sh ${Username}@${Server_IP}:/tmp/httpd_pull.sh
 ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP} /tmp/httpd_pull.sh > ./httpd/pull_result.json
 
-echo ./httpd/pull_result.json
+cat ./httpd/pull_result.json
 
 conffile=$(cat ./pull_result.json | jq .conffile| tr -d '"')
-documentRoot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
+documentroot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
 
 scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./conffile
 scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${documentroot}/* ./documentroot/
