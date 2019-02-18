e4994f4ed66a4dd3439c972382b107d5c83bf59d
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-18 11:23:51 +0530

    update build

--- httpd/build.sh
+++ httpd/build.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
+
+echo -----------------------------------------------------
+echo "        Pulling Configurations and Artifacts"
+echo -----------------------------------------------------
+
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ./httpd/pull.sh ${Username}@${Server_IP}:/tmp/httpd_pull.sh
+ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP} /tmp/httpd_pull.sh > ./httpd/pull_result.json
+
+echo ./httpd/pull_result.json
+
+conffile=$(cat ./pull_result.json | jq .conffile| tr -d '"')
+documentRoot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
+
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./conffile
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${documentroot}/* ./documentroot/
+
 echo -----------------------------------------------------
 echo "        Preparing Docker Image"
 echo -----------------------------------------------------
 
-conffile=$(cat ./pull_result.json | jq .conffile| tr -d '"')
 port=$(cat ./pull_result.json | jq .port| tr -d '"')
-documentRoot=$(cat ./pull_result.json | jq .documentroot| tr -d '"')
-
-sed -i 's:conffile:'"$conffile"':g' Dockerfile
 sed -i 's:port:'"$port"':g' Dockerfile
-sed -i 's:documentRoot:'"$documentRoot"':g' Dockerfile
 
 cat ./httpd/Dockerfile
 docker build . -t httpd_${BUILD_NUMBER}
