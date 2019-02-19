e02c9fba397d4604d2aeee7ac1eb9f2056fe9eae
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-19 14:36:48 +0530

    Update build.sh

--- tomcat/build.sh
+++ tomcat/build.sh
@@ -4,32 +4,28 @@ echo -----------------------------------------------------
 echo "        Pulling Configurations and Artifacts"
 echo -----------------------------------------------------
 
-scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ./httpd/pull.sh ${Username}@${Server_IP}:/tmp/httpd_pull.sh
-ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP} /tmp/httpd_pull.sh > ./httpd/pull_result.json
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ./tomcat/pull.sh ${Username}@${Server_IP}:/tmp/tomcat_pull.sh
+ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP} /tmp/tomcat_pull.sh > ./tomcat/pull_result.json
 
-cat ./httpd/pull_result.json
+cat ./tomcat/pull_result.json
 
-conffile=$(cat ./httpd/pull_result.json | jq .conffile| tr -d '"')
-documentRoot=$(cat ./httpd/pull_result.json | jq .documentRoot| tr -d '"')
+conffile=$(cat ./tomcat/pull_result.json | jq .conffile| tr -d '"')
 
-rm -rf ./httpd/conffile
-scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./httpd/conffile
-rm -rf ./httpd/documentRoot
-mkdir ./httpd/documentRoot
-scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${documentRoot}/* ./httpd/documentRoot/
+rm -rf ./tomcat/conffile
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./tomcat/conffile
 
 echo -----------------------------------------------------
 echo "        Preparing Docker Image"
 echo -----------------------------------------------------
 
-port=$(cat ./httpd/pull_result.json | jq .port| tr -d '"')
-sed -i 's:port:'"$port"':g' ./httpd/Dockerfile
+port=$(cat ./tomcat/pull_result.json | jq .port| tr -d '"')
+sed -i 's:port:'"$port"':g' ./tomcat/Dockerfile
 
-cat ./httpd/Dockerfile
-docker build ./httpd/ -t ${ECR_URL}/g4_hackathon/httpd:${VERSION}
+cat ./tomcat/Dockerfile
+docker build ./tomcat/ -t ${ECR_URL}/g4_hackathon/tomcat:${VERSION}
 
 ./ecr-login ${ECR_ACCESS_KEY} ${ECR_SECRET_KEY} ${ECR_REGION}
 
-docker push ${ECR_URL}/g4_hackathon/httpd:${VERSION}
+docker push ${ECR_URL}/g4_hackathon/tomcat:${VERSION}
 
 docker logout https://${ECR_URL}
