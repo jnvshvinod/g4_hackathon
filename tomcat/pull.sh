be340e60704ecf55917155b0a1be5f74777f14b2
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-19 14:43:33 +0530

    Update pull.sh

--- tomcat/pull.sh
+++ tomcat/pull.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# Script to pull configurations of HTTPD server and build docker image.
+# Script to pull configurations of tomcat server and build docker image.
 
 # Default system properties, Modify for custom locations, if any
-conffile="/etc/httpd/conf/httpd.conf"
+serverxml="/usr/share/tomcat/conf/server.xml"
 
-documentRoot=$(cat /etc/httpd/conf/httpd.conf | grep '^\s*DocumentRoot'|tr -d " "|cut -d'"' -f2)
+port=$(cat $serverxml | grep -e '^\s*Connector port'|tr -d " "|tr -d "Connector port"|head -1)
 
-port=$(cat $conffile | grep -e '^\s*Listen'|tr -d " "|tr -d "Listen"|head -1)
-
-echo "{ \"conffile\":\"${conffile}\",\"documentRoot\":\"${documentRoot}\",\"port\":\"${port}\"}"
+echo "{ \"serverxml\":\"${serverxml}\",\"port\":\"${port}\"}"
