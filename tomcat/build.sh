dc7725cb843a2038dbdde0c42dca9cd35bda3956
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-19 11:14:09 +0530

    Create build.sh

---
+++ tomcat/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo -----------------------------------------------------
+echo "        Pulling Configurations and Artifacts"
+echo -----------------------------------------------------
+
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ./httpd/pull.sh ${Username}@${Server_IP}:/tmp/httpd_pull.sh
+ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP} /tmp/httpd_pull.sh > ./httpd/pull_result.json
+
+cat ./httpd/pull_result.json
+
+conffile=$(cat ./httpd/pull_result.json | jq .conffile| tr -d '"')
+documentRoot=$(cat ./httpd/pull_result.json | jq .documentRoot| tr -d '"')
+
+rm -rf ./httpd/conffile
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./httpd/conffile
+rm -rf ./httpd/documentRoot
+mkdir ./httpd/documentRoot
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${documentRoot}/* ./httpd/documentRoot/
+
+echo -----------------------------------------------------
+echo "        Preparing Docker Image"
+echo -----------------------------------------------------
+
+port=$(cat ./httpd/pull_result.json | jq .port| tr -d '"')
+sed -i 's:port:'"$port"':g' ./httpd/Dockerfile
+
+cat ./httpd/Dockerfile
+docker build ./httpd/ -t ${ECR_URL}/g4_hackathon/httpd:${VERSION}
+
+./ecr-login ${ECR_ACCESS_KEY} ${ECR_SECRET_KEY} ${ECR_REGION}
+
+docker push ${ECR_URL}/g4_hackathon/httpd:${VERSION}
+
+docker logout https://${ECR_URL}
