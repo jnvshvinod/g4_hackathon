9e26517472128b4914b4ea97e3c880ef02d6ba20
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-20 15:21:16 +0530

    Update deploy_on_tomcat.sh

--- deploy_on_tomcat.sh
+++ deploy_on_tomcat.sh
@@ -1,7 +1,8 @@
 #/bin/sh
-wget http://ec2-13-234-48-118.ap-south-1.compute.amazonaws.com:8081/artifactory/list/builds/war/${1}/JavaHelloWorldApp.war
-systemctl stop tomcat
-rm -f /usr/share/tomcat/webapps/JavaHelloWorldApp.war
-rm -rf /usr/share/tomcat/webapps/JavaHelloWorldApp/
-cp ./JavaHelloWorldApp.war /usr/share/tomcat/webapps/
-systemctl start tomcat
+sudo rm -rf Hackathon.war
+wget http://ec2-13-234-48-118.ap-south-1.compute.amazonaws.com:8081/artifactory/list/builds/war/${1}/Hackathon.war
+sudo systemctl stop tomcat
+sudo rm -f /usr/share/tomcat/webapps/Hackathon.war
+sudo rm -rf /usr/share/tomcat/webapps/Hackathon/
+sudo cp ./Hackathon.war /usr/share/tomcat/webapps/
+sudo systemctl start tomcat
