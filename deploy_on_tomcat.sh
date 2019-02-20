e9cf92644876d1fda123200692f32031e3344037
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-20 14:10:09 +0530

    Update deploy_on_tomcat.sh

--- deploy_on_tomcat.sh
+++ deploy_on_tomcat.sh
@@ -2,6 +2,6 @@
 wget http://ec2-13-234-48-118.ap-south-1.compute.amazonaws.com:8081/artifactory/list/builds/war/${1}/JavaHelloWorldApp.war
 sudo systemctl stop tomcat
 sudo rm -f /usr/share/tomcat/webapps/JavaHelloWorldApp.war
-sduo rm -rf /usr/share/tomcat/webapps/JavaHelloWorldApp/
+sudo rm -rf /usr/share/tomcat/webapps/JavaHelloWorldApp/
 sudo cp ./JavaHelloWorldApp.war /usr/share/tomcat/webapps/
 sudo systemctl start tomcat
