b4efbedc89117353603272602007b7486cd9e13f
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-21 15:12:53 +0530

    update webapp

--- tomcat/pull.sh
+++ tomcat/pull.sh
@@ -5,6 +5,8 @@
 # Default system properties, Modify for custom locations, if any
 serverxml="/usr/share/tomcat/conf/server.xml"
 
+webapp="/usr/share/tomcat/webapps/Hackathon.war"
+
 port=$(cat ${serverxml} | grep -e '^\s*<Connector port' | head -1 |tr -d " "| cut -d '=' -f 2 | cut -d '"' -f 2)
 
-echo "{ \"serverxml\":\"${serverxml}\",\"port\":\"${port}\"}"
+echo "{ \"serverxml\":\"${serverxml}\",\"webapp\":\"${webapp}\",\"port\":\"${port}\"}"
