e2cfafba35952cfa6f67be7b36a59f75e40a09c9
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-21 15:37:49 +0530

    update

--- tomcat/build.sh
+++ tomcat/build.sh
@@ -37,9 +37,12 @@ sed -i 's:port:'"$port"':g' ./tomcat/Dockerfile
 
 cat ./tomcat/Dockerfile
 #docker build ./tomcat/ -t ${ECR_URL}/g4_hackathon/tomcat:${VERSION}
-docker build ./tomcat/ -t tomcat:${VERSION}
 
 ./ecr-login ${ECR_ACCESS_KEY} ${ECR_SECRET_KEY} ${ECR_REGION}
+
+docker build ./tomcat/ -t tomcat:${VERSION}
+
+#./ecr-login ${ECR_ACCESS_KEY} ${ECR_SECRET_KEY} ${ECR_REGION}
 
 docker push ${ECR_URL}/g4_hackathon/tomcat:${VERSION}
 
