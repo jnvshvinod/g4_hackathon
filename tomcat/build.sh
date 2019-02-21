72b1c13685cf219948b00599649479feb973d177
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-21 15:28:30 +0530

    Update

--- tomcat/build.sh
+++ tomcat/build.sh
@@ -19,8 +19,14 @@ conffile=$(cat ./tomcat/pull_result.json | jq .serverxml| tr -d '"')
 echo "conffile is" 
 echo $conffile
 
-rm -rf ./tomcat/conffile
+rm -rf ./tomcat/server.xml
 scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${conffile} ./tomcat/server.xml
+
+webapp=$(cat ./tomcat/pull_result.json | jq .webapp| tr -d '"')
+
+rm -rf ./tomcat/server.xml
+mkdir webapps
+scp -i /opt/jenkinsfiles/tmp/bootstrap_key_${Server_IP} -o StrictHostKeyChecking=no ${Username}@${Server_IP}:${webapp} ./tomcat/webapps/
 
 echo -----------------------------------------------------
 echo "        Preparing Docker Image"
