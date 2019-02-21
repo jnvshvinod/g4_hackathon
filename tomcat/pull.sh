e6a57489a501de792971400217b70c93b7e5ae04
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-21 14:51:03 +0530

    update

--- tomcat/pull.sh
+++ tomcat/pull.sh
@@ -5,6 +5,6 @@
 # Default system properties, Modify for custom locations, if any
 serverxml="/usr/share/tomcat/conf/server.xml"
 
-port=$(cat server.xml | grep -e '^\s*<Connector port' | head -1 |tr -d " "| cut -d '=' -f 2 | cut -d '"' -f 2)
+port=$(cat ${serverxml} | grep -e '^\s*<Connector port' | head -1 |tr -d " "| cut -d '=' -f 2 | cut -d '"' -f 2)
 
 echo "{ \"serverxml\":\"${serverxml}\",\"port\":\"${port}\"}"
