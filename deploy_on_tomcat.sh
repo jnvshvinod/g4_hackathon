406b0bdd37da5619c029a064a6f8c9894033c811
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-22 22:15:01 +0530

    Update deploy_on_tomcat.sh

--- deploy_on_tomcat.sh
+++ deploy_on_tomcat.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-sudo rm -rf Hackathon.war
+rm -rf Hackathon.war
 wget http://ec2-13-234-48-118.ap-south-1.compute.amazonaws.com:8081/artifactory/list/builds/war/${1}/Hackathon.war
-sudo systemctl stop tomcat
-sudo rm -f /usr/share/tomcat/webapps/Hackathon.war
-sudo rm -rf /usr/share/tomcat/webapps/Hackathon/
-sudo cp ./Hackathon.war /usr/share/tomcat/webapps/
-sudo systemctl start tomcat
+systemctl stop tomcat
+rm -f /usr/share/tomcat/webapps/Hackathon.war
+rm -rf /usr/share/tomcat/webapps/Hackathon/
+cp ./Hackathon.war /usr/share/tomcat/webapps/
+systemctl start tomcat
