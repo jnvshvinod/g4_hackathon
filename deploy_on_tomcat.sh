265718c6b66acd2e199f36f523405247b6c5efd7
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-27 23:18:11 +0530

    Update deploy_on_tomcat.sh

--- deploy_on_tomcat.sh
+++ deploy_on_tomcat.sh
@@ -1,8 +1,18 @@
 #/bin/sh
-rm -rf Hackathon.war
+echo "Removing old temp artifacts"
+rm -rf ./Hackathon.war
+
+echo "Downloading new artifacts"
 wget http://ec2-13-234-48-118.ap-south-1.compute.amazonaws.com:8081/artifactory/list/builds/war/${1}/Hackathon.war
+
+echo "Stopping tomcat service"
 systemctl stop tomcat
-rm -f /usr/share/tomcat/webapps/Hackathon.war
-rm -rf /usr/share/tomcat/webapps/Hackathon/
+
+echo "Removing old artifacts"
+rm -rf /usr/share/tomcat/webapps/Hackathon/Hackathon.war
+
+echo "Copy new artifacts"
 cp ./Hackathon.war /usr/share/tomcat/webapps/
+
+echo "Restarting tomcat service"
 systemctl start tomcat
