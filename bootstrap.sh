c823686af6123f5237f68b55cfef9ec8feb89070
J N V S H Vinod <vinod.jupudi@gmail.com>
2019-02-28 16:02:43 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ username=$3
 
 # Clean up old keys
 rm -rf /root/.ssh/known_hosts
-rm -rf /opt/jenkinsfiles/tmp/bootstrap_key_${ip}
+rm -rf /opt/jenkinsfiles/tmp/bootstrap_key_${username}_${ip}
 
 echo "Attempting ssh connection to "${username}@${ip}
 
@@ -15,13 +15,13 @@ then
 	password=$4
 	export SSHPASS=$password
 	sshpass -e ssh-copy-id -f -i /opt/jenkinsfiles/bootstrap -o StrictHostKeyChecking=no "${username}"@"${ip}"
-	cp /opt/jenkinsfiles/bootstrap /opt/jenkinsfiles/tmp/bootstrap_key_${ip}
+	cp /opt/jenkinsfiles/bootstrap /opt/jenkinsfiles/tmp/bootstrap_key_${username}_${ip}
 elif [ "${authentication}" == "Username:SSH_Key" ]
 then
-	cp /opt/jenkinsfiles/tmp/SSH_Key /opt/jenkinsfiles/tmp/bootstrap_key_${ip}
+	cp /opt/jenkinsfiles/tmp/SSH_Key /opt/jenkinsfiles/tmp/bootstrap_key_${username}_${ip}
 fi
-chmod 600 /opt/jenkinsfiles/tmp/bootstrap_key_${ip}
-ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${ip} -o StrictHostKeyChecking=no "${username}"@"${ip}" hostname
+chmod 600 /opt/jenkinsfiles/tmp/bootstrap_key_${username}_${ip}
+ssh -i /opt/jenkinsfiles/tmp/bootstrap_key_${username}_${ip} -o StrictHostKeyChecking=no "${username}"@"${ip}" hostname
 
 if [ "$?" == 0 ]
 then
